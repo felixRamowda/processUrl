3320a2da8992dce7d6479443f1146675500629ff
windowFelix <fel123rodriguez@gmail.com>
2024-08-26 13:39:38 -0600

    primer commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('components.App')
+@extends('components.layouts.table');
+
+@section('content')
+    <div class="container">
+       <h1>Hola mundo cambio en tiempo real</h1>
+    </div>  
+@endsection
